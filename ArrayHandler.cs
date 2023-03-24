f803f470e7f79b1255de252ff15cc0d3706a566c
roman lepihov <lepihovrom@yandex.by>
2023-03-24 11:20:45 +0300

    add: methods CreateEmtyArray, FillArray

--- ArrayHandler.cs
+++ ArrayHandler.cs
@@ -5,5 +5,39 @@ using System;
 /// </summary>
 public static class ArrayHandler
 {
+    /// <summary>
+    /// Метод создания массива
+    /// </summary>
+    /// <param name="size"></param>
+    /// <returns>Пустой массив строк</returns>
+    public static string[] CreateEmptyArray(int size)
+    {
+        return new string[size];
+    }
+
+    /// <summary>
+    /// Метод заполнения массива пользователем
+    /// </summary>
+    /// <value>Заполненный пользователем массив</value>
+    public static string[] FillArray(this string [] arr)
+    {
+        for (int i=0; i<arr.Length; i++)
+        {
+            string text=String.Empty;
+
+            while (string.IsNullOrEmpty(text))
+            {
+                Console.Write($"Input a string number {i+1}: ");
+                text = Convert.ToString(Console.ReadLine());
+                if (string.IsNullOrEmpty(text))
+                    Console.Write("String is empty!!! ");
+            }
+            
+            arr[i] = text;
+        }
+        return arr;
+    }
+
+
     
 }
