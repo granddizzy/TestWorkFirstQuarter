438433c39d02bcd46895bc9cf14cedad4f715151
roman lepihov <lepihovrom@yandex.by>
2023-03-24 11:12:25 +0300

    add: input array size, input maxStringSize

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
 ﻿
+using static ArrayHandler;
+
+int size=-1;
+while (size<0)
+{
+    Console.Write("Input array size: ");
+
+    size = Convert.ToInt32(Console.ReadLine());
+    if (size<0)
+        Console.Write("Invalid array size!!! ");
+}
+
+int maxStringSize=-1;
+while (maxStringSize<0)
+{
+    Console.Write("Input maximum string size: ");
+    
+    maxStringSize = Convert.ToInt32(Console.ReadLine());
+    if (maxStringSize<0)
+        Console.Write("Invalid maximum string size!!! ");
+}
