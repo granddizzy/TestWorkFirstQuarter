e029d3369c33cb7a661e99902f9ba9cc58b8385e
roman lepihov <lepihovrom@yandex.by>
2023-03-24 19:57:55 +0300

    update: BlockSchema, README, show array

--- Program.cs
+++ Program.cs
@@ -1,29 +1,30 @@
-﻿
-using static ArrayHandler;
+﻿using static ArrayHandler;
 
 int size=-1;
-while (size<0)
+while (size<=0)
 {
-    Console.Write("Input array size: ");
+    Console.Write("Input array size value above zero: ");
 
     size = Convert.ToInt32(Console.ReadLine());
-    if (size<0)
+    if (size<=0)
         Console.Write("Invalid array size!!! ");
 }
 
 int maxStringSize=-1;
-while (maxStringSize<0)
+while (maxStringSize<=0)
 {
-    Console.Write("Input maximum string size: ");
+    Console.Write("Input maximum string size value above zero: ");
     
     maxStringSize = Convert.ToInt32(Console.ReadLine());
-    if (maxStringSize<0)
+    if (maxStringSize<=0)
         Console.Write("Invalid maximum string size!!! ");
 }
 
 string[] arr = CreateEmptyArray(size).FillArray();
+
+Console.WriteLine("Begin array:");
 arr.ShowArray();
 
-Console.WriteLine("Modify array:");
 string[] newArr = arr.CreateArrayByParameters(maxStringSize);
+Console.WriteLine("Array of strings satisfying the condition: The length of the string is less than :" + maxStringSize);
 newArr.ShowArray();
